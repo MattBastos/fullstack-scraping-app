2dd18d5356651fee9d594645ba98ac2cf554e2b0
MattBastos <matthewsbasttos@gmail.com>
2024-02-24 16:02:23 -0300

    feat: Implements products route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const productRoutes = require('../src/routes/products');
 
 const app = express();
 
 app.use(cors());
+app.use('/api/scrape', productRoutes);
 
 module.exports = app;
