cde50d5106fdcf9dabfdb3b432fc14493ba80441
MattBastos <matthewsbasttos@gmail.com>
2024-02-24 19:56:59 -0300

    fix: product route get function

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const getProducts = require('../controllers/productController');
 
 const productsRoute = express.Router();
 
-productsRoute.get('/', await getProducts(req, res));
+productsRoute.get('/', async (req, res) => {
+  await getProducts(req, res);
+});
+
 
 module.exports = productsRoute;
