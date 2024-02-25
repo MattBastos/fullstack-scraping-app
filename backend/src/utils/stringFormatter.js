affc2ae7dc50812d7be71b7b899affa0af4f2d6d
MattBastos <matthewsbasttos@gmail.com>
2024-02-25 14:11:52 -0300

    docs: Add docs to formatProductRatingAndReviews function

--- backend/src/utils/stringFormatter.js
+++ backend/src/utils/stringFormatter.js
@@ -1,4 +1,10 @@
+/**
+ * Formats the product rating and reviews string.
+ * @param {string} productRatingAndReviews - The original string containing product rating and reviews information.
+ * @returns {string} - The formatted product rating and reviews string.
+ */
 const formatProductRatingAndReviews = (productRatingAndReviews) => {
+  // Remove leading commas, trim leading whitespaces, replace leading 'Avaliação' with 'Avaliação', and remove trailing commas.
   return productRatingAndReviews.replace(/^,*/, '')
   .trimStart()
   .replace(/^Avaliação/, 'Avaliação')
