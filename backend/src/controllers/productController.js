d685b87434023d92bf019e774b0822166546f061
MattBastos <matthewsbasttos@gmail.com>
2024-02-25 14:20:56 -0300

    docs: Add docs to getProducts function

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -17,32 +17,52 @@ const getHTML = async (searchURL) => {
   return data;
 };
 
+
+/**
+ * Retrieves product data based on the provided keyword.
+ * @param {Object} req - Express request object containing query parameters.
+ * @param {Object} res - Express response object to send data back to the client.
+ * @returns {Object} - JSON response containing an array of products with title, rating and reviews, and image.
+ * @throws {Error} - Throws an error if there is an issue with the scraping process or internal server error.
+ */
+
 const getProducts = async (req, res) => {
   try {
+    // Extracting the keyword from the query parameters
     const { keyword } = req.query;
+
+    // Constructing the search URL for Mercado Livre based on the provided keyword
     const searchURL = `${mercadoLivreURL}${keyword}#D[A:${keyword}]`
 
+    // Fetching HTML content from the Mercado Livre search page
     const html = await getHTML(searchURL);
     const $ = cheerio.load(html);
 
+    // Array to store the extracted product data
     const products = [];
 
+    // Iterating through each product element in the first page search results
     $('.ui-search-layout>li').each((_i, el) => {
+      // Extracting product details such as title, rating and reviews, and image URL
       const productTitle = $(el).find('.ui-search-item__title').text();
       const productRatingAndReviews = $(el).find('.andes-visually-hidden').text();
       const productImage = $(el).find('.ui-search-result-image__element').attr('data-src');
 
+      // Creating a product object with extracted data
       const product = {
         title: productTitle,
         ratingAndReviews: formatProductRatingAndReviews(productRatingAndReviews),
         image: productImage
       };
 
+      // Adding the product object to the products array
       products.push(product);
     });
 
+    // Sending the array of products as a JSON response
     res.json(products);
   } catch (error) {
+    // Handling errors and sending an internal server error response
     console.error('Erro:', error);
     res.status(500).send('Erro interno do servidor');
   }
